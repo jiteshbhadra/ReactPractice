487063ccbfe09d10d7f5d84d8611b0ced5af6e27
Jitesh <techjitesh84@gmail.com>
2018-12-09 21:35:04 +0530

    Working with Routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter,Link,Route,Switch} from 'react-router-dom';
+import Header from './components/common/Header';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
 
 class App extends React.Component{
 
     render(){
-        return "<div>First React Component</div>";
+        return (    
+            <BrowserRouter>
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/about" component={AboutPage} />                    
+                </Switch>
+            </div>
+            </BrowserRouter>
+        );
     }
-}
+} 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-console.log("test");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
 
-ReactDOM.render(<App/>,document.getElementById("app"));
+ReactDOM.render(
+<App />
+,document.getElementById("app"));
